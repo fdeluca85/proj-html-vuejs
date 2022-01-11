9c9ccdb1bfd29efed900e3e05c0d8fb4caf2ad25
francesca <fradeluca85@gmail.com>
2022-01-11 13:09:57 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+// import Header from './components/macro/Header.vue';
+// import Main from './components/macro/Main.vue';
+// import Footer from './components/macro/Footer.vue';
+
+export default {
+  // name: 'App',
+  // components: {
+  //     Header,
+  //     Main,
+  //     Footer
+  // },
+  
+}
+</script>
+
+<style lang="scss">
+@import './assets/style/global.scss'
+</style>
