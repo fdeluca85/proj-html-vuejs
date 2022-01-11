44d0752b0e1b3e5328a30625545dfa4e085f1d13
francesca <fradeluca85@gmail.com>
2022-01-11 15:11:24 +0100

    style

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
+    <Header />
+    <Main />
+    <Footer />
     
   </div>
 </template>
 
 <script>
-// import Header from './components/macro/Header.vue';
-// import Main from './components/macro/Main.vue';
-// import Footer from './components/macro/Footer.vue';
+import Header from './components/macro/Header.vue';
+import Main from './components/macro/Main.vue';
+import Footer from './components/macro/Footer.vue';
 
 export default {
-  // name: 'App',
-  // components: {
-  //     Header,
-  //     Main,
-  //     Footer
-  // },
+  name: 'App',
+  components: {
+      Header,
+      Main,
+      Footer
+  },
   
 }
 </script>
